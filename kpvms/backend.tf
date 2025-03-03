337ee10e1262face7cdb6607859855c2ed3a1a4c
Nik <nik@hotmail.com>
2025-03-03 17:48:03 +0530

    updaed SAS

--- kpvms/backend.tf
+++ kpvms/backend.tf
@@ -7,6 +7,6 @@ terraform {
     key                  = "terraform.tfstate"
     subscription_id      = "9bd2fe7b-8da1-4dad-ac7c-7766fe130a05"
     use_azuread_auth   = true
-    sas_token = "sv=2022-11-02&ss=bfqt&srt=co&sp=rwdlacupiytfx&se=2025-02-28T13:44:03Z&st=2025-02-28T05:44:03Z&spr=https&sig=Uqv7EEYdSiqmfmBh47mMnvnA%2FpELAOJXU1urSxWN1xc%3D"
+    sas_token = "sv=2022-11-02&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-03-03T20:12:03Z&st=2025-03-03T12:12:03Z&spr=https,http&sig=kBJyQ5kVZU06u9q49TbCeJH33fOSQxa%2F4wKf9xMRVa8%3D"
   }
 }
